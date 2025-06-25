f338a64016a4e79684f48eba0d019027f34f4eb5
Ubuntu <ubuntu@ip-172-31-15-144.ap-south-1.compute.internal>
2025-06-25 10:22:32 +0000

    Added Dockerfiles, Nginx config, and updated service2 app

--- service_2/app.py
+++ service_2/app.py
@@ -12,6 +12,11 @@ def ping():
 def hello():
     return jsonify(message="Hello from Service 2")
 
+@app.route("/")
+def home():
+    return jsonify(message="Welcome to Service 2")
+
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8002)
